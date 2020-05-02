722dda9dea65e024efeab180fd5aca536a383833
Madara Priede <priede.madara@gmail.com>
2020-05-03 00:15:17 +0300

    Advent day 2

---
+++ Advent2015/Advent2015/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Advent2015
+{
+    class GiftBox
+    {
+        public GiftBox(int length, int width, int height)
+        {
+            this.length = length;
+            this.width = width;
+            this.height = height;
+        }
+
+        public int length { get; set; }
+        public int width { get; set; }
+        public int height { get; set; }
+
+    }
+}
--- Advent2015/Advent2015/Program.cs
+++ Advent2015/Advent2015/Program.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.IO;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace Advent2015
 {
@@ -7,12 +9,53 @@ namespace Advent2015
     {
         static void Main(string[] args)
         {
-            Task1();
+            //Task1();
+            Task2();
         }
 
+        
+        static void Task2()
+        {
+            string fp = @"D:\Jauna mape\C#_Maaciibas\Advente\Advents\Advent2015\box_dimensions.txt";
+            List<string> dimensions = new List<string>();
+            List<GiftBox> boxes = new List<GiftBox>();
+            dimensions = File.ReadAllLines(fp).ToList();
+
+            int sumWrapper = 0;
+            int sumRibbon = 0;
+
+            foreach (string lines in dimensions)
+            {
+                string[] items = lines.Split('x');
+                GiftBox box = new GiftBox(Convert.ToInt32(items[0]), Convert.ToInt32(items[1]), Convert.ToInt32(items[2]));
+                boxes.Add(box);
+            }
+            
+            foreach(GiftBox box in boxes)
+            {
+                int a = box.length,b = box.height, c = box.width;
+
+                int[] parameters = { a, b, c };
+                int longest = parameters.Max();
+                List<int> usables = new List<int>();
+                usables = parameters.ToList();
+                usables.Remove(longest);
+                int ribbon = 2 * usables[0] + 2 *usables[1];
+                Console.WriteLine("Ribbon: " + ribbon);
+                int[] sides = {a*b,b*c,c*a};
+                int smallest = sides.Min();
+                int boxTotal = (2*a*b + 2*b*c + 2*a*c) + smallest ;
+               
+                sumWrapper = sumWrapper + boxTotal;
+                sumRibbon = sumRibbon + a*b*c + ribbon;
+            }
+            Console.WriteLine("total amount for wrapping paper: " + sumWrapper);
+            Console.WriteLine("total amount for ribbon: " + sumRibbon);
+        }
+        
         static void Task1()
         {
-            string fp = @"D:\Jauna mape\C#_Maaciibas\Advente\input_iekavas.txt";
+            string fp = @"D:\Jauna mape\C#_Maaciibas\Advente\Advents\Advent2015\input_iekavas.txt";
             string iekavas = File.ReadAllText(fp);
             int sum = 0;
             int position = 0;
