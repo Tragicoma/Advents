4634fbe0ec2ec768e06bb4acbdef4495803f3a8d
Madara Priede <priede.madara@gmail.com>
2020-05-02 21:03:08 +0300

    1 task of 2015

---
+++ Advent2015/Advent2015/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.IO;
+
+namespace Advent2015
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Task1();
+        }
+
+        static void Task1()
+        {
+            string fp = @"D:\Jauna mape\C#_Maaciibas\Advente\input_iekavas.txt";
+            string iekavas = File.ReadAllText(fp);
+            int sum = 0;
+            int position = 0;
+            
+            foreach(char ch in iekavas)
+            {
+                if(ch == '(')
+                {
+                    sum++;
+                }
+                else if(ch == ')')
+                {
+                    sum--;
+                }
+                position++;
+                if(sum==-1)
+                {
+                    Console.WriteLine("Enters the basement in position: " + position);
+                }
+            }
+
+            Console.WriteLine(" amount of floors: " + sum);
+        }
+
+    }
+}
